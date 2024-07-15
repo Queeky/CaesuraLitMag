b3c67ea0ba7cd7fd03559e3c12200895a9ebb056
Quinn Miersma <quinn.miersma@myemail.indwes.edu>
2024-07-14 21:34:57 -0400

    Updating mobile design for index, header, nav, and footer

---
+++ app/javascript/action.js
@@ -0,0 +1,6 @@
+$(function(){
+    $(".mobile-menu-link").click(function(){
+        $(".mobile-dropdown").toggle();
+        // $(this).css("text-decoration-line") == "none" ? $(this).css("text-decoration-line", "underline") : $(this).css("text-decoration-line", "none"); 
+    });
+ });
