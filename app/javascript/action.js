3658ed48b9a21baa1602ad50395a128b05600066
Quinn Miersma <quinn.miersma@myemail.indwes.edu>
2024-07-27 21:20:01 -0400

    Adding javascript response for mobile dropdown

--- app/javascript/action.js
+++ app/javascript/action.js
@@ -1,6 +1,5 @@
 $(function(){
     $(".mobile-menu-link").click(function(){
         $(".mobile-dropdown").toggle();
-        // $(this).css("text-decoration-line") == "none" ? $(this).css("text-decoration-line", "underline") : $(this).css("text-decoration-line", "none"); 
     });
  });
